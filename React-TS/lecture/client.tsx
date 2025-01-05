3ae458a8e0b8564adefe5b2eaf84d4d08c3ea752
zoe <ddu_buk_e@naver.com>
2025-01-05 17:42:31 +0900

    feat: react-ts initial setting

---
+++ React-TS/lecture/client.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'  // 이제 react import는 안해도 되지 않나
+import * as ReactDOM from 'react-dom' // export default 없으면 애스터리스크 사용해서 import
+import GuGuDan from './GuGuDan'
+
+ReactDOM.render(<GuGuDan />, document.querySelector('#root'))
+
+
+
---
+++ React-TS/lecture/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
+
+  entry: {
+    app: './client'
+  },
+  module: {
+    rules: [{
+      test:/\.tsx?$/,
+      loader: 'ts-loader',
+    }]
+  },
+  plugins: [
+
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
+  }
+}
